e4ec111068f4d91f4bb1b16b92ab03b4f04b9a83
Mujju432 <139369854+Mujju432@users.noreply.github.com>
2023-09-24 20:25:32 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -246,7 +246,7 @@ async def next_page(bot, query):
        InlineKeyboardButton("! Lᴀɴɢᴜᴀɢᴇs !", callback_data=f"select_lang#{req}")
     ])
     btn.insert(0, [
-        InlineKeyboardButton("⚡ How To Download ⚡", url=f"https://t.me/TheMoviesUpdate/130"),
+        InlineKeyboardButton("⚡How To Download⚡", url=f"https://t.me/TheMoviesUpdate/130"),
         InlineKeyboardButton("⚡ Cʜᴇᴄᴋ Bᴏᴛ PM ⚡", url=f"https://t.me/{temp.U_NAME}")
     ])
     try:
@@ -359,7 +359,7 @@ async def language_check(bot, query):
         ])
 
         btn.insert(0, [
-            InlineKeyboardButton("⚡ How To Download ⚡", url=f"https://t.me/TheMoviesUpdate/130"),
+            InlineKeyboardButton("⚡How To Download⚡", url=f"https://t.me/TheMoviesUpdate/130"),
             InlineKeyboardButton("⚡ Cʜᴇᴄᴋ Bᴏᴛ PM ⚡", url=f"https://t.me/{temp.U_NAME}")
         ])
 
@@ -1627,7 +1627,7 @@ async def auto_filter(client, msg, spoll=False):
     ])
 
     btn.insert(0, [
-        InlineKeyboardButton("⚡ How To Download ⚡", url=f"https://t.me/TheMoviesUpdate/130"),
+        InlineKeyboardButton("⚡How To Download⚡", url=f"https://t.me/TheMoviesUpdate/130"),
         InlineKeyboardButton("⚡ Cʜᴇᴄᴋ Bᴏᴛ PM ⚡", url=f"https://t.me/{temp.U_NAME}")
     ])
 
